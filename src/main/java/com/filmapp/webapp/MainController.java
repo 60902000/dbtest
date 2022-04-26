e4720641fc7e38a6b1d78b8c25d559078191ba34
yshshc <yshshc@gmail.com>
2022-04-26 12:22:57 +0900

    create spring boot project

---
+++ src/main/java/com/filmapp/webapp/MainController.java
@@ -0,0 +1,13 @@
+package com.filmapp.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String showHomePage() {
+        System.out.println("main controller");
+        return "index";
+    }
+}
